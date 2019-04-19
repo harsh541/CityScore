9f8370d96ce0ae69ca869413b542846fe7059a00
kevinxia787 <kevinxia787@gmail.com>
2019-04-18 22:41:53 -0400

    added insertion to DB script and updated backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,61 +1,35 @@
 import requests
 import json
-import requests
-import os
-
+import sqlite3
+from sqlite3 import Error
 from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from calculate_scores import pothole_scores
-from datetime import datetime
+from flask import g
+
+DATABASE = 'scores.db'
 
 app = Flask(__name__)
 CORS(app)
-# db_path = os.path.join(os.path.dirname(__file__), 'xlab.db')
-# db_uri = 'sqlite:///{}'.format(db_path)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
 
-class ScoreData(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  date = db.Column(db.String(120), unique=False, nullable=False)
-  topic = db.Column(db.String(120), unique=True, nullable=False)
-  day = db.Column(db.Float, unique=False, nullable=True)
-  month = db.Column(db.Float, unique=False, nullable=True)
-  year = db.Column(db.Float, unique=False, nullable=True)
-
-  def __init__(self, date, topic, day, month, year):
-    self.date = date
-    self.topic = topic
-    self.day = day
-    self.month = year
-    self.year = year
-
-def add_row():
-  (pothole_day, pothole_month, pothole_year) = pothole_scores()
-  pothole_score_data = ScoreData(datetime.now().strftime("%Y-%m-%d %H:%M"), 'does this work', pothole_day, pothole_month, pothole_year)
-  db.session.add(pothole_score_data)
-  db.session.commit()
-
+def query_db(query, args=(), one=False):
+  conn = sqlite3.connect(DATABASE)
+  curr = conn.execute(query)
+  rv = curr.fetchall()
+  curr.close()
+  return(rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def get_all():
-  score_data = ScoreData.query.all()
   result = []
-  for row in score_data:
-    curr = {'id': row.id, 'topic': row.topic, 'day': row.day, 'month': row.month, 'year': row.year}
-    result.append(curr)
-  result2 = {"passed": True}
-  json_result = json.dumps(result)
-  print(result)
-  return json_result
+  for row in query_db('select * from score_data'):
+    (_id, date, topic, day, month, year) = row
+    obj_data = {'topic': topic, 'day':day, 'month': month, 'year': year}
+    result.append(obj_data)
+
+  data = json.dumps(result)
+  return data
 
 
-
-if __name__ == '__main__':
-  # db.create_all()
-  add_row()
-  
 
 
 
